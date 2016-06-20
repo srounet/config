9749a89a8d28597af9638e3d87efe492be0ea71b
Fabien Reboia <srounet@gmail.com>
2016-06-20 16:09:30 +0200

    add script to install vim dependencies

---
+++ install_vim.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s ~/config/.tmux.conf ~/.
+ln -s ~/config/.vim ~/.
+ln -s ~/config/.vimrc ~/.
+ln -s ~/config/.profile ~/.
+ln -s ~/config/.bashrc ~/.
+ln -s ~/config/.profile ~/.
+ln -s ~/config/.bash_aliases ~/.
